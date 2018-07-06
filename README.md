64c474357ffd36a035ba374ebf8413edd4228f55
patrick.jaja <patrickjaja@web.de>
2018-07-07 01:53:50 +0200

    init

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 Max Performance = No Configuration, No Tools, No Libs = Use the Platform and win
 
-Gettings Started
- 
 ````php -S localhost:8000 router.php````
 
--- src/App/Modules/Greeting/Business/Greeting.mjs
+++ src/App/Modules/Greeting/Business/Greeting.mjs
@@ -1,7 +1,3 @@
 import { Greeting } from "../Communication/Greeting.js";
 
 document.querySelector('#app').innerHTML = '<custom-greeting></custom-greeting>';
-
-export function say (name) {
-    console.log(name);
-}
